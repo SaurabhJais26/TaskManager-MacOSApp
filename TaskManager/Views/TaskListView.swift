676ccf1e93b792c49817d724c0271699301885f1
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-12-25 11:33:05 +0530

    Added all model classes.

---
+++ TaskManager/Views/TaskListView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListView.swift
+//  TaskManager
+//
+//  Created by Saurabh Jaiswal on 25/12/24.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListView()
+}
